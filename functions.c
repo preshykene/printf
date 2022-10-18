85904d5f085a8a4a2d4e2c85f3ccd9235a5fed8e
ChinweIjy1 <casuoha@gmail.com>
2022-10-18 02:04:44 -0700

    Function and main resolved

--- functions.c
+++ functions.c
@@ -1,7 +1,6 @@
 #include "main.h"
-#include <stdarg.h>
-/************************* PRINT CHAR *************************/
 
+/************************* PRINT CHAR *************************/
 /**
  * print_char - Prints a char
  * @types: List a of arguments
@@ -13,11 +12,11 @@
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-char c = va_arg(types, int);
+	char c = va_arg(types, int);
 
-return (handle_write_char(c, buffer, flags, width, precision, size));
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 /************************* PRINT A STRING *************************/
 /**
@@ -31,48 +30,47 @@ return (handle_write_char(c, buffer, flags, width, precision, size));
  * Return: Number of chars printed
  */
 int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-int length = 0, i;
-char *str = va_arg(types, char *)
+	int length = 0, i;
+	char *str = va_arg(types, char *);
 
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
-if (str == NULL)
-{
-str = "(null)";
-if (precision >= 6)
-str = "      ";
-}
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
+	while (str[length] != '\0')
+		length++;
 
-while (str[length] != '\0')
-length++;
+	if (precision >= 0 && precision < length)
+		length = precision;
 
-if (precision >= 0 && precision < length)
-length = precision;
+	if (width > length)
+	{
+		if (flags & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
 
-if (width > length)
-{
-if (flags & F_MINUS)
-{
-write(1, &str[0], length);
-for (i = width - length; i > 0; i--)
-write(1, " ", 1);
-return (width);
-}
-else
-{
-for (i = width - length; i > 0; i--)
-write(1, " ", 1);
-write(1, &str[0], length);
-return (width);
-}
-}
-
-return (write(1, str, length));
+	return (write(1, str, length));
 }
 /************************* PRINT PERCENT SIGN *************************/
 /**
@@ -86,15 +84,15 @@ return (write(1, str, length));
  * Return: Number of chars printed
  */
 int print_percent(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-UNUSED(types);
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
-return (write(1, "%%", 1));
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
 }
 
 /************************* PRINT INT *************************/
@@ -109,36 +107,33 @@ return (write(1, "%%", 1));
  * Return: Number of chars printed
  */
 int print_int(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-int i = BUFF_SIZE - 2;
-int is_negative = 0;
-long int n = va_arg(types, long int);
-unsigned long int num;
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
 
-n = convert_size_number(n, size);
+	n = convert_size_number(n, size);
+	if (n == 0)
+		buffer[i--] = '0';
 
-if (n == 0)
-buffer[i--] = '0';
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
 
-buffer[BUFF_SIZE - 1] = '\0';
-num = (unsigned long int)n;
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
 
-if (n < 0)
-{
-num = (unsigned long int)((-1) * n);
-is_negative = 1;
-}
-
-while (num > 0)
-{
-buffer[i--] = (num % 10) + '0';
-num /= 10;
-}
-
-i++;
-
-return (write_number(is_negative, i, buffer, flags, width, precision, size));
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+	i++;
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
 /************************* PRINT BINARY *************************/
@@ -153,7 +148,7 @@ return (write_number(is_negative, i, buffer, flags, width, precision, size));
  * Return: Numbers of char printed.
  */
 int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
